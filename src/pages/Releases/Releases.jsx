54d7ffa563711f871c3e96522a699fd94daa2e59
Matthew Stinis <mpstinis@gmail.com>
2022-02-26 21:57:24 -0500

    add releases page base markup and styles

--- src/pages/Releases/Releases.jsx
+++ src/pages/Releases/Releases.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import mockData from "./mockData.json";
 
 import './Releases.scss'
 
 const Releases = () => {
     return (
-        <div>Releases</div>
+        <div className="releases">
+            <div className="releases__grid">
+                {mockData.map(entry => (
+                    <div className="releases__item" key={entry.id}>
+                        <img src={entry.imgUrl} alt="cat" />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
