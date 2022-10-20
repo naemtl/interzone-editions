67984946f699128b2498e4980eda5a7c92698e01
M. Stinis <mpstinis@gmail.com>
2022-10-20 00:23:34 -0400

    Releases page (#2)
    * add releases page base markup and styles
    
    * add Homepage and Navbar component files

--- src/pages/Releases/Releases.jsx
+++ src/pages/Releases/Releases.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import mockData from "./release-database.json";
 
 import './Releases.scss'
 
 const Releases = () => {
     return (
-        <div>Releases</div>
+        <div className="releases">
+            <h1>TEST</h1>
+            <div className="releases__grid">
+                {mockData.map(entry => (
+                    <div className="releases__item" key={entry.id}>
+                        <img src={entry.imgUrl} alt="cat" />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
